78f54df85277a495ce7f12638c4c0422f7d1ed46
wangchengwen <319217500@qq.com>
2020-11-24 20:12:00 +0800

    本地工程添加git

---
+++ gittest/src/com/git/git.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
